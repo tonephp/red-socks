33f7875e7b6c03415e6da5be284e57776576aecf
Andrii Pereverziev <a.pereverziev@mail.enamine.net>
2021-04-18 21:06:34 +0300

    Update gitignore and hero component

--- app/components/hero/hero.php
+++ app/components/hero/hero.php
@@ -1,8 +1,12 @@
-<div class="hero">  
-  <div class="hero__icon">
-    <?=icon('tonephp-logo')?>
-  </div>
-  <div class="hero__logo-text">
-    TonePHP
+<div class="hero">
+  <div class="hero__body">
+    <div class="hero__logo">
+      <div class="hero__icon">
+        <?=icon('tonephp-logo')?>
+      </div>
+      <div class="hero__logo-text">
+        TonePHP
+      </div>
+    </div>
   </div>
 </div>
