f21be6f6c2709efd4f2a43a42a06b917ec85f71c
tonephp <77553087+tonephp@users.noreply.github.com>
2021-05-06 20:03:21 +0300

    Update Tone.php

--- core/Tone.php
+++ core/Tone.php
@@ -10,21 +10,45 @@ class Tone {
 
   public function __construct() {
     session_start();
+
+    self::init();
     self::$app = Registry::instance();
     $this->getParams();
     new ErrorHandler;
+    self::dispatchRouter();
   }
 
-  protected function getParams(){
+  protected static function init() {
+    define('WWW', __DIR__ . '/../../../../public');
+    define('ROOT', dirname(__DIR__ . '/../../../../..'));
+    define('CORE', ROOT . '/core');
+    define('CONFIG', ROOT . '/config');
+    define('APP', ROOT . '/app');
+    define('CACHE', ROOT . '/tmp/cache');
+    define('LAYOUT', 'default');
+
+    $dotenv = \Dotenv\Dotenv::createImmutable(ROOT);
+    $dotenv->load();
+
+    define("DEBUG", $_ENV['DEBUG']);
+
+    require __DIR__ . '/functions.php';
+    require APP . '/functions.php';
+    require APP . '/routes.php';
+  }
+
+  protected static function dispatchRouter() {
+    $query = $_SERVER['QUERY_STRING'];
+    $query = rtrim($query, '/');
+    Router::dispatch($query);
+  }
+
+  protected function getParams() {
     $params = require_once CONFIG . '/params.php';
     if(!empty($params)){
         foreach($params as $k => $v){
             self::$app->setProperty($k, $v);
         }
     }
-  }
-
-  public static function requireFunctions() {
-    require __DIR__ . '/functions.php';
   }
 }
